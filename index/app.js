cb416731f3f4304bd531e327572baf2c1e554336
ManuelGonzalez-S <125252015+ManuelGonzalez-S@users.noreply.github.com>
2023-03-28 09:41:19 +0200

    Add files via upload

--- index/app.js
+++ index/app.js
@@ -19,8 +19,7 @@ function cargarPagina(){
 // Genera seccion de noticias
 function generarNoticias(){
 
-    let section = document.createElement('section');
-    section.setAttribute('id','noticias');
+    let section = document.getElementById('noticias');
 
     let div;
 
@@ -71,8 +70,7 @@ function generarTablaClasificacionPilotos(){
 
     let titulos = ['Clasificación','Piloto','Puntos','Equipo'];
  
-    let section = document.createElement('section');;
-    section.setAttribute('id','clasificacion');
+    let section = document.getElementById('clasificacion');
 
     let tabla = document.createElement('table');
     tabla.setAttribute('id','tablaClasificacion')
@@ -151,8 +149,7 @@ function devolverNombresPilotos(){
 // Genera la seccion de equipos
 function mostrarEquipos(){
 
-    let section = document.createElement('section');
-    section.setAttribute('id','equipos');
+    let section = document.getElementById('equipos');
 
     let card;
 
@@ -339,7 +336,7 @@ function generarCopyright(){
     div.setAttribute('id','copyright');
 
     let p = document.createElement('p');
-    p.innerHTML = '© Copyright';
+    p.innerHTML = '© Copyright 2023';
 
     div.appendChild(p);
 
