95555c9f4e3cdfde19cf32beb6569b51b96a5f02
Dennis Hidalgo <124583417+Dennis-Hidalgo@users.noreply.github.com>
2023-03-29 14:19:34 +0200

    Add files via upload
    falta validaciones login

--- login/app.js
+++ login/app.js
@@ -7,17 +7,17 @@ let formulario1 = formularios[0];
 let formulario2 = formularios[1];
 let valMail = /^\w+@[a-zA-Z]+[.][[a-zA-Z]+$/;
 let valPass = /(?=.*\d{2})(?=.*[A-Z])+((?=.*[. - _ , =])+).{7,}/;
-let valString = /^[a-zA-Z]{,20}$/;
+let valString = /^[a-zA-Z]{3,25}$/;
 
 
-function registrar(){
+function registrar() {
     main.removeChild(admin);
     main.appendChild(formulario2);
     main.removeChild(formulario1);
     main.appendChild(admin);
 }
 
-function volverLogin(){
+function volverLogin() {
     main.removeChild(admin);
     main.appendChild(formulario1);
     main.removeChild(formulario2);
@@ -25,12 +25,12 @@ function volverLogin(){
 
 }
 
-function cargarLogin(){
+function cargarLogin() {
     main.removeChild(formulario2);
 }
 
 
-function getDatosLogin(){
+function getDatosLogin() {
     email = formulario1['elements']['emailL']['value'];
     validarMail();
     pass = formulario1['elements']['passL']['value'];
@@ -38,10 +38,8 @@ function getDatosLogin(){
     console.log(pass)
 }
 
-function hacerRegistro(){
-    nombre =formulario2['elements']['nombreR']['value'];
-    formulario1[0].classList.add('correcto') ;
-    validarMail(0);
+function hacerRegistro() {
+    nombre = formulario2['elements']['nombreR']['value'];
     apellido = formulario2['elements']['apellidoR']['value'];
     email = formulario2['elements']['emailR']['value'];
     pass = formulario2['elements']['passR']['value'];
@@ -49,18 +47,18 @@ function hacerRegistro(){
 
 }
 
-function validarString(){
-    if (valString.test(formulario2['elements']['nombreR']['value'])){
-        formulario2[0].classList.remove('incorrecto');
-        formulario2[0].classList.add('correcto');
+function validarString(indice, nombre) {
+    if (valString.test(formulario2['elements'][nombre]['value'])) {
+        formulario2[indice].classList.remove('incorrecto');
+        formulario2[indice].classList.add('correcto');
     } else {
-        formulario2[0].classList.remove('correcto');
-        formulario2[0].classList.add('incorrecto');
-}
+        formulario2[indice].classList.remove('correcto');
+        formulario2[indice].classList.add('incorrecto');
+    }   
 }
 
-function validarMail(indice){
-    if (valPass.test(email)){
+function validarMail(indice) {
+    if (valMail.test(formulario2['elements']['emailR']['value'])) {
         formulario2[indice].classList.remove('incorrecto');
         formulario2[indice].classList.add('correcto');
     } else {
@@ -69,4 +67,35 @@ function validarMail(indice){
     }
 }
 
-formulario1[0].classList.add('correcto');
+function validarPass(indice, n, nombre){
+    if (valPass.test(formularios[n]['elements'][nombre]['value'])) {
+        formularios[n][indice].classList.remove('incorrecto');
+        formularios[n][indice].classList.add('correcto');
+    } else {
+        formularios[n][indice].classList.remove('correcto');
+        formularios[n][indice].classList.add('incorrecto');
+    }
+    if (indice == 3){
+        // validarRepetirPass(indice);
+        validarRepetirPass(indice+1);
+    }
+}
+
+function validarRepetirPass(indice){
+    mensaje = document.getElementsByTagName('span')[0];
+    if (!(formulario2['elements']['passR']['value'] == formulario2['elements']['pass2R']['value'])){
+        mensaje.innerHTML = 'Las contraseñas no coinciden';
+        formulario2[indice].classList.remove('correcto');
+        formulario2[indice].classList.add('incorrecto');
+    } else {
+        formulario2[indice].classList.remove('incorrecto');
+        formulario2[indice].classList.add('correcto');
+        mensaje.innerHTML = ""
+    }
+    if (indice == 4){
+        validarPass(indice);
+    }
+
+}
+
+// formulario1[0].classList.add('correcto');
