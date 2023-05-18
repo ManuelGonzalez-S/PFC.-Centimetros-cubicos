02bc1472d0d9c1228c8ed21d2b69ad7b3ea835f1
ManuelGonzalez-S <125252015+ManuelGonzalez-S@users.noreply.github.com>
2023-05-18 14:19:27 +0200

    Add files via upload

--- database.php
+++ database.php
@@ -1,56 +1,79 @@
 <?php
 
-    class database{
+class database
+{
 
-        public static function conectar(){
-            $driver = 'mysql'; // Que tipo de base de datos voy a utilizar
-            $host = 'localhost';
-            $port = '3306';
-            $bd = 'centimetroscubicos';
-            $user = 'root'; //Cambiar a uno nuevo con permisos
-            $password = ''; // Esto tambien cambia
+    public static function conectar()
+    {
+        $driver = 'mysql'; // Que tipo de base de datos voy a utilizar
+        $host = 'localhost';
+        $port = '3306';
+        $bd = 'centimetroscubicos';
+        $user = 'root'; //Cambiar a uno nuevo con permisos
+        $password = ''; // Esto tambien cambia
 
-            $dsn = "$driver:dbname=$bd;host=$host;port=$port";
+        $dsn = "$driver:dbname=$bd;host=$host;port=$port";
 
-            try {
-                $gbd = new PDO($dsn, $user, $password);
-            } catch (PDOException $e) {
-                echo 'Falló la conexion; ' . $e->getMessage();
-            }
-
-            return $gbd;
+        try {
+            $gbd = new PDO($dsn, $user, $password);
+        } catch (PDOException $e) {
+            echo 'Falló la conexion; ' . $e->getMessage();
         }
 
-        public function getTabla($tabla){
+        return $gbd;
+    }
+
+    public function getTabla($tabla)
+    {
+        if ($tabla != null) {
             $sql = "SELECT * FROM $tabla";
             $resultados = self::conectar()->query($sql);
             return $resultados;
         }
-
-        function getPilotosEquipos(){
-
-            $sql = "select pilotos.*,equipos.nombre as 'nombreEquipo' from pilotos inner join equipos on equipos.id = pilotos.equipos_id order by pilotos.Puntos desc;";
-            $resultados = self::conectar()->query($sql);
-            return $resultados;
-        
-        }
-
-        function getTablaenBaseAID($tabla,$id,$limit){
-            $sql = "select * from $tabla where id = $id limit $limit;";
-            $resultados = self::conectar()->query($sql);
-            return $resultados;
-        }
-
-        function getTablaSegunCampoID($tabla,$campo,$id,$limit){
-            $sql = "select * from $tabla where $campo = $id limit $limit;";
-            $resultados = self::conectar()->query($sql);
-            return $resultados;
-        }
-
-        function getNumeroEquipos(){
-            $sql = "select count(*) as 'suma' from equipos;";
-            $resultados = self::conectar()->query($sql);
-            return $resultados;
-        }
-
     }
+
+    public function getTablaToArray($tabla)
+    {
+        if ($tabla != null) {
+            $sql = "SELECT * FROM $tabla";
+            $resultados = self::conectar()->query($sql);
+            return $resultados->fetchAll(PDO::FETCH_ASSOC);
+        }
+    }
+
+    function getPilotosEquipos()
+    {
+
+        $sql = "select pilotos.*,equipos.nombre as 'nombreEquipo' from pilotos inner join equipos on equipos.id = pilotos.equipos_id order by pilotos.Puntos desc;";
+        $resultados = self::conectar()->query($sql);
+        return $resultados;
+    }
+
+    function getTablaenBaseAID($tabla, $id, $limit)
+    {
+        $sql = "select * from $tabla where id = $id limit $limit;";
+        $resultados = self::conectar()->query($sql);
+        return $resultados;
+    }
+
+    function getTablaSegunCampoID($tabla, $campo, $id, $limit)
+    {
+        $sql = "select * from $tabla where $campo = $id limit $limit;";
+        $resultados = self::conectar()->query($sql);
+        return $resultados;
+    }
+
+    function getNumeroEquipos()
+    {
+        $sql = "select count(*) as 'suma' from equipos;";
+        $resultados = self::conectar()->query($sql);
+        return $resultados;
+    }
+
+    function getEquiposClasi()
+    {
+        $sql = 'select * from equipos order by puntos desc;';
+        $resultados = self::conectar()->query($sql);
+        return $resultados;
+    }
+}
